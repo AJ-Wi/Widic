1a7ef2f8300089e9450257ffb28fa7552770c940
ajwipo <tropaguararia28@gmail.com>
2022-07-25 00:13:53 -0500

    added configuration and environment variable for api key

---
+++ src/config.js
@@ -0,0 +1,12 @@
+// @ts-check
+"use strict";
+
+require("dotenv").config();
+module.exports = {
+  SCHCOOL_API_KEY: process.env.SCHCOOL_API_KEY,
+  SPANISH_API_KEY: process.env.SPANISH_API_KEY,
+  SCHCOOL_API_URL: "https://www.dictionaryapi.com/api/v3/references/sd4/json/",
+  SPANISH_API_URL: "https://www.dictionaryapi.com/api/v3/references/spanish/json/",
+  AUDIO_API_URL: "https://media.merriam-webster.com/audio/prons/en/us/mp3/",
+  BEGINS: [/\bbix/, /\bgg/, /^[#$%^&*\-_~0-9]/],
+};
