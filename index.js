3e20779ab7b9306ac0df7228454049805ef23756
ajwipo <tropaguararia28@gmail.com>
2022-07-24 22:36:28 -0500

    Startup of small app to consume English Dictionary API

---
+++ index.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+// @ts-check
+
+"use strict";
+
+console.log("Hello, world!");
+
+module.exports = 123;
---
+++ test/index.test.js
@@ -0,0 +1,5 @@
+const index = require("../index");
+
+test("index", () => {
+  expect(index).toBe(123);
+});
