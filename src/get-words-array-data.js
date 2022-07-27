7fa615c1ace2244504bc11176e06ff6684f4ee66
ajwipo <tropaguararia28@gmail.com>
2022-07-27 02:18:01 -0500

    passed the recursion to the get-word-data module and added output of words not found

--- src/get-words-array-data.js
+++ src/get-words-array-data.js
@@ -1,6 +1,7 @@
 // @ts-check
 "use strict";
 const getWordData = require("./get-word-data.js");
+const msg = require("./message.js");
 
 /**
  * get words array data.
@@ -14,25 +15,29 @@ const getWordData = require("./get-word-data.js");
  *
  * @param {array} words - words array.
  * @param {number} length - length of words array or length needed.
- * @returns {Promise<array>} words array data.
+ * @returns {Promise<object>} words object data.
  */
 const getWordsArrayData = async (words, length) => {
   const arrayData = [];
-  console.log("Getting words array data...");
+  const errorWords = [];
+  msg.info("Getting words array data...");
   try {
     for (let i = 0; i < length; i++) {
       let data = await getWordData(words[i].word);
-      if (data.word !== words[i].word) {
-        data = await getWordData(data.word);
+      if (data instanceof String) {
+        msg.error("Error getting word data: " + data);
+        errorWords.push(words[i].word);
+        continue;
       }
-      console.log(`processing new word data: ${i} `, data);
+
+      msg.info(`processing new word data: ${i} `, data);
       arrayData.push(data);
     }
-    console.log("Done getting words array data!");
-    return arrayData;
+    msg.info("Done getting words array data!");
+    return { arrayData, errorWords };
   } catch (err) {
-    console.log("getWordsArrayData ", err);
-    return arrayData;
+    msg.error("getWordsArrayData ", err);
+    return { arrayData, errorWords };
   }
 };
 
