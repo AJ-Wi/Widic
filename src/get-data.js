bc548773ce7011c2346fc012770fb3eba0f0d605
ajwipo <tropaguararia28@gmail.com>
2022-07-27 00:25:49 -0500

    extracting getdata to a separate module and correcting function names

---
+++ src/get-data.js
@@ -0,0 +1,22 @@
+// @ts-check
+"use strict";
+
+/**
+ * Get the data from the url
+ *
+ * Example url
+ *
+ * https://www.dictionaryapi.com/api/v3/references/sd4/json/baseball?key=you_api_key
+ *
+ * @param {string} url - the url to get the data
+ * @returns {Promise<any>}
+ */
+const getData = async (url = "") => {
+  const response = await fetch(url);
+  if (response.ok) {
+    return response.json();
+  }
+  throw new Error(response.statusText);
+};
+
+module.exports = getData;
--- src/get-word-data.js
+++ src/get-word-data.js
@@ -1,33 +1,16 @@
 // @ts-check
 
 "use strict";
+const getData = require("./get-data.js");
 const { SCHCOOL_API_URL, SCHCOOL_API_KEY } = require("./config.js");
 const { checkText, checkID } = require("./utility.js");
-
-/**
- * Get the data from the url
- *
- * Example url
- *
- * https://www.dictionaryapi.com/api/v3/references/sd4/json/baseball?key=you_api_key
- *
- * @param {string} url - the url to get the data
- * @returns {Promise<any>}
- */
-const getData = async (url = "") => {
-  const response = await fetch(url);
-  if (response.ok) {
-    return response.json();
-  }
-  throw new Error(response.statusText);
-};
 
 /**
  * Get the data for the word from the schcool api.
  * @param {string} word - the word to get the data
  * @returns {Promise<object>}
  */
-const getDataWord = async (word) => {
+const getWordData = async (word) => {
   if (checkText(word)) return checkText(word);
 
   const data = await getData(SCHCOOL_API_URL + word + "?key=" + SCHCOOL_API_KEY);
@@ -45,11 +28,11 @@ const getDataWord = async (word) => {
 
     if (item.hwi.prs !== undefined && wID === word) {
       IPA = item.hwi.prs[0].mw;
-      audio = item.hwi.prs[0].audio;
+      audio = item.hwi.prs[0].sound.audio;
       definition = item.shortdef[0];
     }
   });
   return { word, IPA, definition, audio };
 };
 
-module.exports = getDataWord;
+module.exports = getWordData;
