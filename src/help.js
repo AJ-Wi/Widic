077882e124611b42c041c82a89454d45080479d9
ajwipo <tropaguararia28@gmail.com>
2022-07-31 01:42:37 -0500

    Update help

--- src/help.js
+++ src/help.js
@@ -1,10 +1,26 @@
-module.exports = `
-  Usage: widic [options] [arguments]
+module.exports = `\x1b[34mwidic - a simple dictionary\x1b[0m
+\x1b[34m***************************\x1b[0m
+A simple implementation to consume the API from https://www.dictionaryapi.com/
 
-  Options:
-    -c, --config                    print config file
+\x1b[32mUsage:\x1b[0m
+\x1b[32m======\x1b[0m
+    widic [options] [input] [arguments]
+
+\x1b[32mOptions:\x1b[0m
+\x1b[32m--------\x1b[0m
     -h, --help                      print widic command line options (currently set)
-    -s, --silent                    silent mode (no output) (default: false) 
-    -v, --vesion                    print widic version
-    -w, --word                      word to get the IPA
+    -v, --version                   print widic version
+    -s, --silent                    silent mode (no output) (default: false)
+    -t, --translate                 translate text (default: false)
+
+\x1b[32minput:\x1b[0m
+\x1b[32m------\x1b[0m
+    -w, --word <word>               the word to get data for (default: "hello")
+    -f, --file <file>               the file to get data for (must be a JSON file)
+
+\x1b[32mArguments:\x1b[0m
+\x1b[32m----------\x1b[0m
+    -l, --length <length>           the number of words to search for (default: array length)
+    -of, --output-file              output file (default: ./widic-output.json)
+    -oa, --output-audio             download audio files (default: ./)
 `;
