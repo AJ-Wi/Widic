42c9df77882f76cce1d5a86506721cbcb323ae2d
ajwipo <tropaguararia28@gmail.com>
2022-07-31 01:43:03 -0500

    add new flags

--- src/get-flags.js
+++ src/get-flags.js
@@ -8,15 +8,42 @@
  */
 const getFlags = (arg) => {
   const flags = [];
-  let flagTemp = "";
 
   for (let i = 2; i < arg.length; i++) {
     if (arg[i] === "-v" || arg[i] === "--version") flags.push("-v");
     if (arg[i] === "-h" || arg[i] === "--help") flags.push("-h");
     if (arg[i] === "-s" || arg[i] === "--silent") flags.push("-s");
-    if (arg[i] === "-c" || arg[i] === "--config") flags.push("-c");
     if (arg[i] === "-w" || arg[i] === "--word") flags.push("-w");
-    if (flags[flags.length - 1] === "-w" && arg[i].substring(0, 1) !== "-") flags.push(arg[i]);
+    if (arg[i] === "-f" || arg[i] === "--file") flags.push("-f");
+    if (arg[i] === "-d" || arg[i] === "--download") flags.push("-d");
+    if (arg[i] === "-t" || arg[i] === "--translate") flags.push("-t");
+    if (arg[i] === "-l" || arg[i] === "--length") flags.push("-l");
+    if (arg[i] === "-of" || arg[i] === "--output-file") flags.push("-of");
+    if (arg[i] === "-oa" || arg[i] === "--output-audio") flags.push("-oa");
+    if (flags[flags.length - 1] === "-w" && arg[i].substring(0, 1) !== "-") {
+      flags.push(arg[i]);
+      continue;
+    }
+    if (flags[flags.length - 1] === "-f" && arg[i].substring(0, 1) !== "-") {
+      flags.push(arg[i]);
+      continue;
+    }
+    if (flags[flags.length - 1] === "-l" && arg[i].substring(0, 1) !== "-") {
+      flags.push(arg[i]);
+      continue;
+    }
+    if (flags[flags.length - 1] === "-of" && arg[i].substring(0, 1) !== "-") {
+      flags.push(arg[i]);
+      continue;
+    }
+    if (flags[flags.length - 1] === "-oa" && arg[i].substring(0, 1) !== "-") {
+      flags.push(arg[i]);
+      continue;
+    }
+  }
+  if (flags.length === 0) {
+    flags.push("-w");
+    flags.push("hello");
   }
   return flags;
 };
