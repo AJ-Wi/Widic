b4699e658cb8b66c8d76144a31d27a0057b48de6
ajwipo <tropaguararia28@gmail.com>
2022-11-12 23:57:29 -0500

    update

--- example/index.js
+++ example/index.js
@@ -1,7 +1,7 @@
 // @ts-check
 "use strict";
 const widic = require("../src/index");
-const words = require("../data/words.json");
+const words = require("../data/basic.json");
 
 const getAllWords = async (words, length) => {
   const wordsArrayData = await widic.getAll(words, length);
@@ -10,8 +10,13 @@ const getAllWords = async (words, length) => {
 
 getAllWords(words, words.length)
   .then((wordsArrayData) => {
-    widic.writeFile(wordsArrayData.arrayData, "wordsWithIPA.json");
+    let newArray = [];
+    wordsArrayData.arrayData.forEach((data) => {
+      newArray.push({ word: data.word, audio: `[sound:${data.audio}.mp3]`, translation: "", IPA: data.IPA });
+    });
+    widic.writeFile(newArray, "wordsWithIPA.json");
     widic.writeFile(wordsArrayData.errorWords, "errorWords.json");
+    widic.downloadAudioFilesFromArray(wordsArrayData.arrayData, "../audio/");
   })
   .catch((err) => {
     console.log(err);
